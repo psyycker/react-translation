49aaa46fc275ff5c6c99e57c1e8d2f92ba0f6e1f
Remy Villulles <remy.villulles@gmail.com>
2021-04-20 20:58:20 +0100

    Put under construction on the homepage

--- documentation/src/components/HomepageFeatures.js
+++ documentation/src/components/HomepageFeatures.js
@@ -53,11 +53,7 @@ export default function HomepageFeatures() {
   return (
     <section className={styles.features}>
       <div className="container">
-        <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
-        </div>
+        <h1>Documentation website under construction</h1>
       </div>
     </section>
   );
